5df218ab4e99c06737ba42a1370d466d23567e2c
dmytrkat <dmytrkat@fel.cvut.cz>
2023-05-24 22:49:47 +0200

    Added possibility to enable/disable loggers when the game is started.

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,28 @@ import View.GamePannel;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        boolean enableLogging = false;
+
+        System.out.print("Enable logging? (yes/no): ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if (input.equalsIgnoreCase("yes")) {
+            enableLogging = true;
+        }
+
+        if (enableLogging) {
+            configureLogging(Level.INFO);
+        } else {
+            configureLogging(Level.OFF);
+        }
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
@@ -21,5 +40,15 @@ public class Main {
         gamepanel.setupGame();
         gamepanel.startGameThread();
 
+    }
+
+    private static void configureLogging(Level level) {
+        Logger rootLogger = Logger.getLogger("");
+        rootLogger.setLevel(level);
+
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(level);
+
+        rootLogger.addHandler(consoleHandler);
     }
 }
